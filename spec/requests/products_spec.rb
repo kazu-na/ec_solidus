8612cbef395f418be8813e77b245f5c04916f6c8
kazu_na <kazuhide.n428@gmail.com>
2019-11-10 08:49:47 +0900

    Add rspec

---
+++ spec/requests/products_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "ProductsRequests", type: :request do
+  describe "GET #show" do
+    let(:product) { create(:product) }
+    before do
+      get potepan_product_path(product.id)
+    end
+
+    it "正常なレスポンス" do
+      expect(response).to have_http_status(200)
+    end
+
+    describe 'productが値を取得できているか' do
+      let(:product) { create(:product, name: 'foobar') }
+
+      it '値がページ中に含まれていること' do
+        expect(response.body).to include 'foobar'
+      end
+    end
+  end
+end
---
+++ spec/system/products_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "ProductsSystems", type: :system do
+  describe "product pages" do
+    let(:product) { create(:product) }
+    before do
+      visit potepan_product_path(product.id)
+    end
+
+    it "商品名が表示される" do
+      expect(page).to have_content product.name
+    end
+
+    it "商品価格が表示される" do
+      expect(page).to have_content product.display_price
+    end
+
+    it "商品説明が表示される" do
+      expect(page).to have_content product.description
+    end
+  end
+end
