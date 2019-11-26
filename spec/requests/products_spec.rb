e429d0d4e39d6e1c6a5ee98f9e0ea157fd49b682
kazu_na <kazuhide.n428@gmail.com>
2019-11-26 15:37:19 +0900

    Add Products_spec

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "ProductsRequests", type: :request do
   describe "GET #show" do
-    let(:product) { create(:product) }
+    let(:taxon)             { create(:taxon) }
+    let!(:product)          { create(:product, name: 'TOTE', taxons: [taxon]) }
+    let(:product_etc)       { create(:product, name: 'STATUE') }
+    let(:related_products)  { create_list(:product, 20, taxons: [taxon]) }
 
     before do
       get potepan_product_path(product.id)
@@ -12,12 +15,17 @@ RSpec.describe "ProductsRequests", type: :request do
       expect(response).to have_http_status(200)
     end
 
-    describe 'productが値を取得できているか' do
-      let(:product) { create(:product, name: 'foobar') }
+    it 'productが値を取得できているか' do
+      expect(response.body).to include 'TOTE'
+    end
 
-      it '値がページ中に含まれていること' do
-        expect(response.body).to include 'foobar'
-      end
+    it '関連していないproductが含まれていないか' do
+      expect(response.body).not_to include 'STATUE'
+    end
+
+    it "関連数が13以上であっても上限設定数の12になっているか" do
+      expect(related_products.count).to eq 20
+      expect(product.related_products.count).to eq 12
     end
   end
 end
