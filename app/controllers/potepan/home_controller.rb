5615673f52425ac2a02f59d2d645fe7ac5377689
kazu_na <kazuhide.n428@gmail.com>
2019-12-06 23:47:44 +0900

    new_productsの実装と細かな修正

--- app/controllers/potepan/home_controller.rb
+++ app/controllers/potepan/home_controller.rb
@@ -1,4 +1,6 @@
 class Potepan::HomeController < ApplicationController
+  MAX_LATEST_ITEM_COUNT = 12
   def index
+    @new_products = Spree::Product.add_price_image.latest_order(MAX_LATEST_ITEM_COUNT)
   end
 end
