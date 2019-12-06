a8c861ed0b608988205c2199610c758e9e582e84
kazu_na <kazuhide.n428@gmail.com>
2019-12-06 13:34:38 +0900

    訂正 ファイル名をtopからindexへ, Decoratorを戻しbaseでscopeを取得

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,10 +1,14 @@
-Spree::Product.class_eval do
-  scope :add_price_image, -> { includes(master: [:default_price, :images]) }
-  scope :latest_order, -> (count) { order(available_on: :desc).limit(count) }
+module Spree::ProductDecorator
+  def self.prepended(base)
+    base.scope :add_price_image, -> { includes(master: [:default_price, :images]) }
+    base.scope :latest_order, -> (count) { order(available_on: :desc).limit(count) }
+  end
+
   def related_products
     Spree::Product.in_taxons(taxons).
       add_price_image.
       where.not(id: id).
       distinct
   end
+  Spree::Product.prepend self
 end
