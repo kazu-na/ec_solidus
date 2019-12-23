6b4db0c38797a124a002e746f814efbd6a1cefee
kazu_na <kazuhide.n428@gmail.com>
2019-12-23 23:49:32 +0900

    scope :sort_by_orderの修正

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -5,13 +5,13 @@ Spree::Product.class_eval do
   scope :sort_by_order,   -> (sort) do
     case sort
     when "NEW_PRODUCTS"
-      new_products
+      reorder(nil).new_products
     when "OLD_PRODUCTS"
-      old_products
+      reorder(nil).old_products
     when "LOW_PRICE"
-      ascend_by_master_price
+      unscope(:order).ascend_by_master_price
     when "HIGH_PRICE"
-      descend_by_master_price
+      unscope(:order).descend_by_master_price
     end
   end
   scope :search_option,   -> (option_value) do
