b5c058df39c26fe4a780be1f927a1e117d611105
kazu_na <kazuhide.n428@gmail.com>
2020-01-11 00:17:27 +0900

    scope sort_by_orderを修正し、ソート時の画像表示順序を修正

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,16 +2,19 @@ Spree::Product.class_eval do
   scope :add_price_image, -> { includes(master: [:default_price, :images]) }
   scope :new_products,    -> { order(available_on: :desc) }
   scope :old_products,    -> { order(available_on: :asc) }
+  scope :position_order,  -> { order("spree_assets.position" => :asc) }
   scope :sort_by_order,   -> (sort) do
     case sort
     when "NEW_PRODUCTS"
-      reorder(nil).new_products
+      reorder(nil).new_products.position_order
     when "OLD_PRODUCTS"
-      reorder(nil).old_products
+      reorder(nil).old_products.position_order
     when "LOW_PRICE"
-      unscope(:order).ascend_by_master_price
+      unscope(:order).ascend_by_master_price.position_order
     when "HIGH_PRICE"
-      unscope(:order).descend_by_master_price
+      unscope(:order).descend_by_master_price.position_order
+    else
+      reorder(nil).new_products.position_order
     end
   end
   scope :search_option,   -> (option_value) do
